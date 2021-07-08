f6b5f22d88178bb4b9f7cd151213b8f70e7bad4a
robin <80066142+robinnlmn@users.noreply.github.com>
2021-07-08 19:49:00 +0200

    some stuff

---
+++ components/Post.js
@@ -0,0 +1,31 @@
+import styles from '../styles/Card.module.css'
+import Image from 'next/image'
+
+import RepostIcon from '@material-ui/icons/Replay';
+import LikeIcon from '@material-ui/icons/ThumbUp';
+
+export default function Post({ title, img, author, desc, likes, reposts }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.header}>
+                    <h2>{title}</h2>
+                    <a>{author}</a>
+                </div>
+                <Image src={img} alt="post" width='100%' height='100%' layout='responsive' priority />
+                <div className={styles.feedback}>
+                    <div className={styles.flex}>
+                        <LikeIcon className={styles.icon} />
+                        <h5>{likes}</h5>
+                    </div>
+
+                    <div className={styles.flex}>
+                        <RepostIcon className={styles.icon} />
+                        <h5>{reposts}</h5>
+                    </div>
+                </div>
+                <h5>{desc}</h5>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button'
+
+import Post from '../components/Post'
+
+import HomeIcon from '@material-ui/icons/Home';
+import AccountIcon from '@material-ui/icons/AccountCircle';
+import LogoutIcon from '@material-ui/icons/ExitToApp';
 
 export default function Home() {
   return (
@@ -12,7 +19,55 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        TEST
+        <div className={styles.left}>
+          <div className={styles.button}>
+            <HomeIcon className={styles.icon} />
+            <h3>HOME</h3>
+          </div>
+
+          <div className={styles.button}>
+            <AccountIcon className={styles.icon} />
+            <h3>PROFILE</h3>
+          </div>
+
+          <div className={styles.button}>
+            <LogoutIcon className={styles.icon} />
+            <h3>LOGOUT</h3>
+          </div>
+        </div>
+
+        <div className={styles.middle}>
+          <Post
+            title="TEST"
+            author="EARNABLE"
+            desc="This is a description This is a description This is a description This is a description This is a description This is a description"
+            likes="1512"
+            reposts="45"
+            img="https://i.pinimg.com/564x/8e/d9/b2/8ed9b2fbc6f2135fbbb844cc0604f721.jpg"
+          />
+
+          <Post
+            title="TEST"
+            author="EARNABLE"
+            desc="This is a description This is a description This is a description This is a description This is a description This is a description"
+            likes="1512"
+            reposts="45"
+            img="https://i.pinimg.com/564x/8e/d9/b2/8ed9b2fbc6f2135fbbb844cc0604f721.jpg"
+          />
+
+          <Post
+            title="TEST"
+            author="EARNABLE"
+            desc="This is a description This is a description This is a description This is a description This is a description This is a description"
+            likes="1512"
+            reposts="45"
+            img="https://i.pinimg.com/564x/8e/d9/b2/8ed9b2fbc6f2135fbbb844cc0604f721.jpg"
+          />
+        </div>
+
+        <div className={styles.right}>
+          YOU CAN SEE WHO YOU ARE FOLLOWING
+        </div>
       </main>
     </div>
   )
